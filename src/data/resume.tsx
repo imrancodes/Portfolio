652f4577da1e5afecba9a82a217dc44df007877b
Imran Khan <imrankhanshakeel012@gmail.com>
2025-12-02 15:44:37 +0530

    resume update

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -1,7 +1,7 @@
 import { Icons } from "@/components/icons";
 import { Contact, HomeIcon, Quote, View } from "lucide-react";
 const resumeUrl =
-  "https://drive.google.com/file/d/1b-U3hhR0dIhZ_wVLlPHw00bAMFuul2Bd/view";
+  "https://drive.google.com/file/d/1tR-Iv7HeEnFSyDJeoh56NsUSYrRRS_ks/view";
 export const DATA = {
   name: "Imran Khan",
   initials: "IK",
